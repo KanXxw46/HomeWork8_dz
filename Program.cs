208b39a2128fbdd3e38cf4de2abb6353c2598f50
KanXxw46 <1kan_andreikkkk@mail.ru>
2020-06-14 22:45:41 +0600

    2 task

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeWork8_Dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
